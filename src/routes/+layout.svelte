0f8c1e3dd9b3099915a940d0568ebfa586f23193
NISHA NAJIHAH <nishanajihah88@gmail.com>
2026-07-17 01:39:02 +0800

    feat: Add and Update favicon and implement to the route/layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-	import "../app.scss";
-	import favicon from "$lib/assets/favicon.svg";
+  import "../app.scss";
+  import faviconIco from "$lib/assets/favicon-nn/favicon.ico";
+  import faviconPng from "$lib/assets/favicon-nn/favicon-96x96.png";
+  import appleTouchIcon from "$lib/assets/favicon-nn/apple-touch-icon.png";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <!-- Legacy fallback -->
+  <link rel="icon" href={faviconIco} sizes="any" />
+
+  <!-- High-res PNG favicon (96x96) -->
+  <link rel="icon" href={faviconPng} type="image/png" sizes="96x96" />
+
+  <!-- iOS App Icon -->
+  <link rel="apple-touch-icon" href={appleTouchIcon} />
 </svelte:head>
 
 {@render children()}
